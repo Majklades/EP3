25e800d3d68540d80b61bde88e78f68311daa989
Michal Polák <113165392+Majklades@users.noreply.github.com>
2022-09-09 08:59:19 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
-if __name__=="__main__":
+if __name__ == '__main__':
     a=input()
-    b=input()
     a=int(a)
+    b=input()
     b=int(b)
-    c=(a*b)
+    c=int((2*a+b*3)/2)
+    d=4
     for i in range(0,4,1):
-        print(a*b)
-    if a>=5:
-        p=int(a/2)
-    else:
-        p=a
-    if b<5:
-        o=int(b/4)
-    else:
-        o=int(b)
-    for i in range(0,p,1):
-        for j in range(0,o,1):
-            if c%2==0:
-                print("*",end="")
-            else:
-                print("/",end="")
+        print(c)
+    if c%5==0:
+        for j in range(0,a/2,1):
+            print("*",end="")
         print()
+    if c%5==2:
+        for j in range(0,a/2,1):
+            print("*", end="")
+        print()
+    if c%5==5:
+        for j in range(0,a/2,1):
+            print("*", end="")
+        print()
+    else:
+        for j in range(0,a,1):
+            print("*",end="")
+        print()
+    if b>=6:
+        for i in range(b/2):
+            print("*",end="")
+        print()
+    else:
+        for i in range(0,b,1):
+            print("*",end="")
+        print()
+
+print("Dnes se citim na znamku:",d)
+
 
 
